cce16debc7f015eca7632bf308ababa373dd3be1
BoopYurNose <zacharybaca07@gmail.com>
2025-12-18 16:24:16 +0000

    :wq

--- CheckList.lua
+++ CheckList.lua
@@ -1,8 +1,15 @@
 local ModuleList = require("ModuleList")
 
 function MainMenu()
-    print("Test")
+    local CurrentDate = os.date("%d-%m-%Y")
+    print("The current date is: "..CurrentDate)
+    
+    for Task, Completed in pairs(ModuleList.AllListItems) do
+        print(Task" "..tostring(Completed))
+        
+    end
 
+    print("\n Type in one of the options below to do them \n")
 end
 
 MainMenu()
--- ModuleList.lua
+++ ModuleList.lua
@@ -1,9 +1,11 @@
 ModuleList = {
-    AllListItems = {"Test"}, -- Every List item you input
+    AllListItems = {Test = false}, -- Every List item you input
 
-    InProgressListItems = {}, -- Lists/Tasks that aren't marked as complete
+    --[[Instead of hardcoding each List item to a table of either complete or incomplete I'll assign a bolean value of false
+    to each List item indicating that it's not complete. When it's complete the bolean value will be changed to true
 
-    CompletedListItems = {}, -- Lists/tasks marked as done
+    ]]
+    Options = {"AddTask", "RemoveTask", "ViewList"}
 }
 
 return ModuleList
