91620bcf79650b14d8501ee565a78c66adf066d6
BoopYurNose <zacharybaca07@gmail.com>
2025-12-18 16:42:40 +0000

    :wq

--- ModuleList.lua
+++ ModuleList.lua
@@ -1,5 +1,5 @@
 ModuleList = {
-    AllListItems = {Test = false}, -- Every List item you input
+    AllListItems = {Test = false}, -- Every List item you input --[[Figure out how to have an existing string key with a string attached (the task) and a bolean value assigned to it]]
 
     --[[Instead of hardcoding each List item to a table of either complete or incomplete I'll assign a bolean value of false
     to each List item indicating that it's not complete. When it's complete the bolean value will be changed to true
