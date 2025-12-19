6a00c5c86a8a754530aa56c48d40518bf8d5d8fb
BoopYurNose <zacharybaca07@gmail.com>
2025-12-19 20:43:57 +0000

    :wq

--- CheckList.lua
+++ CheckList.lua
@@ -1,12 +1,13 @@
 local ModuleList = require("ModuleList")
 
 function MainMenu()
-    local CurrentDate = os.date("%d-%m-%Y")
+    local CurrentDate = os.date("%m-%d-%Y")
     print("The current date is: "..CurrentDate)
     
     for Task, Completed in pairs(ModuleList.AllListItems) do
-        print(Task" "..tostring(Completed))
         
+        print(Task.." "..tostring(Completed))
+
     end
 
     print("\n Type in one of the options below to do them \n")
